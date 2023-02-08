938ea0a1b60f714bdaa4bf3c0ef796ef841085a3
LeoValleWeb <leomartinsvalle@gmail.com>
2023-02-08 16:44:31 +0000

    save

---
+++ Variaveis.java
@@ -0,0 +1,11 @@
+public class Variaveis{
+    public static void main(String[] args){
+        int idade=20;
+        byte quantidadeBola=5;
+        char letra='M';
+        boolean vivo=false;
+        double salario = 1340.50;
+        float salarioMenor = 1200.00f;
+        short alunosPalmital = 10000;
+        long populacaoPalmital = 22000;
+        System.out.println(idade);}}

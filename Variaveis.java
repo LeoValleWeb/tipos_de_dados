02afcd7335cb185f41295ca5a2bf599aea7e9660
LeoValleWeb <leomartinsvalle@gmail.com>
2023-02-08 17:04:56 +0000

    save2

--- Variaveis.java
+++ Variaveis.java
@@ -1,6 +1,6 @@
 public class Variaveis{
     public static void main(String[] args){
-        int idade=20;
+        int idade=16;
         byte quantidadeBola=5;
         char letra='M';
         boolean vivo=false;
@@ -8,4 +8,18 @@ public class Variaveis{
         float salarioMenor = 1200.00f;
         short alunosPalmital = 10000;
         long populacaoPalmital = 22000;
-        System.out.println(idade);}}
+
+        String nome;
+        nome="Leonardo Valle";
+
+        System.out.println("Meu nome é "+nome +" tenho " +idade+"anos");
+        System.out.println(idade);
+        System.out.println(quantidadeBola);
+        System.out.println(letra);
+        System.out.println(vivo);
+        System.out.println(salario);
+        System.out.println(salarioMenor);
+        System.out.println(alunosPalmital);
+        System.out.println(populacaoPalmital);
+    
+    }}

9eafc1219aa5b30231c3c54ead5aa1e92ba5eea9
LeoValleWeb <leomartinsvalle@gmail.com>
2023-02-14 14:11:24 +0000

    save3

--- Operadores.java
+++ Operadores.java
@@ -1,3 +1,14 @@
 public class Operadores {
-    
+   public static void main(String[] leleo) {
+
+        int idade;
+        idade = 16;
+        idade++;
+        idade++;
+        System.out.println("Eu tenho "+ idade +" anos");
+        idade+=10;
+        System.out.println(idade);
+
+   }
+
 }
